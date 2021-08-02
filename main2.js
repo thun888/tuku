b03e6dbdb66335251766b9b5952e0b9e1eb0eed8
thun888 <63234268+thun888@users.noreply.github.com>
2021-08-02 10:08:16 +0800

    Update and rename main1.js to main2.js

--- main2.js
+++ main2.js
@@ -4,26 +4,23 @@ let res
         alert("url不能为空！")
         return
     }
-    if(document.querySelector("#dir").value==""){
     document.getElementById("searchbtn").disabled=true;
 	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Please wait...';
+  if(document.querySelector("#dir").value=="")
     fetch(window.location.pathname, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ url: document.querySelector("#text").value })
-    }).then(function(response) {return response.json();})
-  } else {
-    document.getElementById("searchbtn").disabled=true;
-    document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Please wait...';
+    }).then(function(response) {
+      return response.json();
+    })
+    if(document.querySelector("#dir").value!=="")
       fetch(window.location.pathname, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ url: document.querySelector("#text").value,dir: document.querySelector("#dir").value,cust: 1 })
-      }).then(function(response) {
-      return response.json();
-})
-  }
-  
+        body: JSON.stringify({ url: document.querySelector("#text").value,dir: document.querySelector("#dir").value,cust: "1" })
+      }).then(function(response) {return response.json();})
+
   .then(function(myJson) {
     res = myJson;
     document.getElementById("searchbtn").disabled=false;
