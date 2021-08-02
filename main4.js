f9c0a710e748c070b78e36c0a1606f74c03719be
thun888 <63234268+thun888@users.noreply.github.com>
2021-08-02 10:16:13 +0800

    Update and rename main3.js to main4.js

--- main4.js
+++ main4.js
@@ -6,18 +6,18 @@ let res
     }
     document.getElementById("searchbtn").disabled=true;
 	document.getElementById("searchbtn").innerHTML='<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Please wait...';
-  if(document.querySelector("#dir").value=="")
-    fetch(window.location.pathname, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url: document.querySelector("#text").value })
-    }).then(function(response) {return response.json();})
+  if(document.querySelector("#dir").value==!"")
+  fetch(window.location.pathname, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url: document.querySelector("#text").value,dir: document.querySelector("#dir").value,cust: "1" })
+  }).then(function(response) {return response.json();})
   else
-    fetch(window.location.pathname, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url: document.querySelector("#text").value,dir: document.querySelector("#dir").value,cust: "1" })
-    }).then(function(response) {return response.json();})
+  fetch(window.location.pathname, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url: document.querySelector("#text").value })
+  }).then(function(response) {return response.json();})
 
   .then(function(myJson) {
     res = myJson;
